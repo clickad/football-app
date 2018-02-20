46b75a71584456fd56153f8101ad7e7c8bed1235
Adnan Damadzic <adnan.damadzic@gmail.com>
2018-02-20 11:08:43 +0430

    Add files via upload

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,9 @@
 var app = angular.module("scores", ['ngRoute']);
 
+app.run(function($rootScope) {
+  $rootScope.root = 'file:///C:/Users/dam37153/Desktop/pr/libary/portf/pages/scores/';
+})
+
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
