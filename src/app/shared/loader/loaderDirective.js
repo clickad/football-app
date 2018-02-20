a0819461717f717d083f3097706c9596e0ac88b5
Adnan Damadzic <adnan.damadzic@gmail.com>
2018-02-20 11:08:15 +0430

    standing directive

---
+++ src/app/shared/loader/loaderDirective.js
@@ -0,0 +1,26 @@
+﻿angular
+  .module('scores')
+  .directive('loader', loader);
+
+function loader($rootScope, $http, $timeout) {
+  var vm = this;
+  vm.root = $rootScope.root;
+  return {
+    restrict: 'A',
+    replace: true,
+    templateUrl: vm.root + "app/shared/loader/loaderView.html",
+    link: function (scope, elm, attrs){
+      scope.isLoading = function () {
+        return $http.pendingRequests.length > 0;
+      };
+
+      scope.$watch(scope.isLoading, function (v){
+        if(v){
+          $(elm).show();
+        }else{
+          $(elm).hide();
+        }
+      });
+    }
+  };
+}
---
+++ src/app/shared/standingsDirective.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('scores');
+app.directive('standings', standings)
+
+function standings($http, $rootScope, standingsService, $timeout) {
+  var vm = this;
+  vm.root = $rootScope.root;
+  return {
+    restrict: "A",
+    scope:{
+      stand: "="
+    },
+    templateUrl: vm.root + "app/shared/table.html"
+  };
+}
