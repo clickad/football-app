ac7749cbb8fe2f9bcc5e23fd49b108b7286400da
Adnan Damadzic <adnan.damadzic@gmail.com>
2018-02-20 11:07:22 +0430

    changed to https

--- src/app/services/competitionsService.js
+++ src/app/services/competitionsService.js
@@ -1,32 +1,26 @@
 ﻿angular
   .module('scores')
   .factory('competitionsService', competitionsService);
-  
-  competitionsService.$inject = ['$http', '$q'];
-
-    function competitionsService($http, $q) {
-
-       var service = {
-            competitions: [],
-            getCompetitions: getCompetitions
-       };
-       return service;
-
-       function getCompetitions() {
-           var def = $q.defer();
-
-           $http({method : 'GET',
-             url : 'http://api.football-data.org/v1/competitions', 
-	     headers: { 'X-Auth-Token': '92c212ebba4842ef95b0958290d908ce'}
-             })
-             .success(function(data, status) {
-               service.competitions = data;
-               def.resolve(data);
-             })
-             .error(function(data, status) {
-             alert("There is a problem with your network, please try again....");
-             })
-
-            return def.promise;
-        }
+competitionsService.$inject = ['$http', '$q'];
+function competitionsService($http, $q) {
+  var service = {
+    competitions: [],
+    getCompetitions: getCompetitions
+  };
+  return service;
+  function getCompetitions() {
+    var def = $q.defer();
+    $http({method : 'GET',
+           url : 'https://api.football-data.org/v1/competitions', 
+           headers: { 'X-Auth-Token': '92c212ebba4842ef95b0958290d908ce'}
+          })
+      .success(function(data, status) {
+      service.competitions = data;
+      def.resolve(data);
+    })
+      .error(function(data, status) {
+      alert("There is a problem with your network, please try again....");
+    })
+    return def.promise;
   }
+}
